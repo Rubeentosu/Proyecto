d433eb117675f4f62f011dd41d81175f6c994dc4
unknown <javillena13@gmail.com>
2024-05-13 13:14:33 +0200

    Terminado extra2, faltan pruebas

--- src/Proyecto/Grupo.java
+++ src/Proyecto/Grupo.java
@@ -99,49 +99,81 @@ public class Grupo {
     //Función que nos imprime el dinero que le tiene que dar quien a quien para igualar las cuentas
     public String ajusteDeCuentas(){
         String cadena="";
-        double menorDeuda=Double.MAX_VALUE;
-        double menorEnElQueCabe=(-1)*Double.MAX_VALUE;
+        double menorDeuda=(-1)*Double.MAX_VALUE;
+        double menorEnElQueCabe=Double.MAX_VALUE;
         Map<usuario, Double> saldos= verSaldo(); //Creamos el mapa con los saldos de cada usuario
         //Mientras alguien con deudas
         while(saldos.size()>1){
             //Eliminamos del mapa todos aquellos que tengan sus deudas ya saldadas
-            eliminarEntrada(saldos);
+            eliminarCeros(saldos);
             //Recorremos los valores y buscamos el que tenga la deuda (positiva) más pequeña
             for (Double n : saldos.values()){
-                if(n>0 && n<menorDeuda) menorDeuda=n;
+                if(n<0 && n>menorDeuda) menorDeuda=n;
             }
             //una vez tengamos el valor, buscamos la persona que deba recibir dinero, con la cantidad de dinero
             //más pequeña que nos permita meter la cantidad anterior
             for (Double n : saldos.values()){
-                if(n<0 && n>menorEnElQueCabe && (n+menorDeuda)<=0) menorEnElQueCabe=n;
+                if(n>0 && n<menorEnElQueCabe && (n+menorDeuda)<=0) menorEnElQueCabe=n;
             }
             //Una vez tenemos la cantidad a abonar y a la cantidad a la que se le suma, debemos sacar los usuarios que tienen los valores
             double d1=menorDeuda;
             double d2 = menorEnElQueCabe;
 
-            usuario pagador = saldos.entrySet().stream()
-                    .filter(entrada -> entrada.getValue().equals(d1))
-                    .findFirst().get().getKey();
+            //Si el pagador puede darle toda la cantidad del tiron a otro, hacemos:
+            if (d2 != Double.MAX_VALUE){
+                usuario pagador = saldos.entrySet().stream()
+                        .filter(entrada -> entrada.getValue().equals(d1))
+                        .findFirst().get().getKey();
 
-            usuario pagado = saldos.entrySet().stream()
-                    .filter(entrada -> entrada.getValue().equals(d2))
-                    .findFirst().get().getKey();
+                usuario pagado = saldos.entrySet().stream()
+                        .filter(entrada -> entrada.getValue().equals(d2))
+                        .findFirst().get().getKey();
 
-            //Metemos en la cadena los valores
-            cadena = cadena + pagador.getName()+" paga " + menorDeuda;
+                //Metemos en la cadena los valores
+                cadena = cadena + pagador.getName()+" paga " + menorDeuda*(-1) + "a ------> " + pagado.getName() + "\n";
 
-            //Ahora, sobreescribimos los valores en el mapa metiendo la misma key y el valor resultante del traspaso de dinero
-            //El que paga pasa a tener un valor de 0 porque ha pagado toda su deuda a la misma persona
-            saldos.put(pagador, 0.0);
-            //Al que recibe el dinero se le hace la cuenta
-            saldos.put(pagado, menorEnElQueCabe+menorDeuda);
+                //Ahora, sobreescribimos los valores en el mapa metiendo la misma key y el valor resultante del traspaso de dinero
+                //El que paga pasa a tener un valor de 0 porque ha pagado toda su deuda a la misma persona
+                saldos.put(pagador, 0.0);
+                //Al que recibe el dinero se le hace la cuenta
+                saldos.put(pagado, menorEnElQueCabe+menorDeuda);
+            }
+            else{
+                //Sacamos el usuario que tiene que repartir el dinero entre varios
+                usuario repartidor = saldos.entrySet().stream()
+                        .filter(entrada -> entrada.getValue().equals(d1))
+                        .findFirst().get().getKey();
+                //Con un do-while, mientras que siga teniendo deuda, irá repartiendo entre los siguientes que tengan que recibir dinero
+                do{
+                    //Buscamos el primer usuario que tenga que recibir dinero
+                    usuario recibe = saldos.entrySet().stream()
+                            .filter( u -> u.getValue() > 0.0)
+                            .findFirst().get().getKey();
+                    //Si la deuda es mayor que lo que tiene que recibir, metemos el que recibe con nuevo valor 0, y el que
+                    //manda con la diferencia
+                    if(saldos.get(repartidor) + saldos.get(recibe) <= 0){
+                        cadena = cadena + repartidor.getName()+" paga " + saldos.get(recibe) + "a ------> " + recibe.getName() + "\n";
+                        saldos.put(repartidor,saldos.get(repartidor) + saldos.get(recibe));
+                        saldos.put(recibe, 0.0);
+                    }
+                    else{
+                        cadena = cadena + repartidor.getName()+" paga " + saldos.get(recibe) + "a ------> " + recibe.getName() + "\n";
+                        saldos.put(repartidor,0.0);
+                        saldos.put(recibe, saldos.get(repartidor) + saldos.get(recibe));
+                    }
+
+
+                }while(saldos.get(repartidor) != 0.0);
+
+            }
+
 
         }
         return cadena;
     }
 
     //Función que elimina del mapa de deudas todos aquellas claves que tengan un saldo/deuda de 0
-    public void eliminarEntrada(Map<usuario, Double> mapa){
+    public void eliminarCeros(Map<usuario, Double> mapa){
         Iterator<Map.Entry<usuario, Double>> it = mapa.entrySet().iterator();
         while (it.hasNext()){
             Map.Entry<usuario,Double> entrada = it.next();
@@ -167,4 +199,6 @@ public class Grupo {
                 .mapToDouble(Gasto::getCantidad)//Sacamos la cantidad
                 .sum(); //Hacemos la sumatoria
     }
+
+
 }
